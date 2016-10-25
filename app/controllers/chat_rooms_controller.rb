d03f7d61d83149721529651012dae3099c953afd
mindziukasss <mindziukasss@gmail.com>
2016-10-25 22:08:11 +0300

    Added normalize,new styling

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,4 +1,5 @@
 class ChatRoomsController < ApplicationController
+  before_action :find_chat_room, only: [:show, :edit, :update, :destroy]
   def index
     @chat_rooms = ChatRoom.all
   end
@@ -22,9 +23,18 @@ class ChatRoomsController < ApplicationController
     @message = Message.new
   end
 
+  def destroy
+    @chat_room.destroy
+    redirect_to root_path
+  end
+
   private
 
   def chat_room_params
     params.require(:chat_room).permit(:title)
+  end
+
+  def find_chat_room
+    @chat_room = ChatRoom.find_by(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chat_rooms, only: [:new, :create, :show, :index]
+  resources :chat_rooms, only: [:new, :create, :show, :index, :destroy]
   root 'chat_rooms#index'
 
   mount ActionCable.server => '/cable'
